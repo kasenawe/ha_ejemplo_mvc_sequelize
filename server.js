13a2cd1981cec749f6e62ad2e9bb98d41fbf9ffe
dievaliente <dievaliente@gmail.com>
2023-05-09 18:02:29 -0300

    inicio passport

--- server.js
+++ server.js
@@ -17,3 +17,27 @@ app.listen(APP_PORT, () => {
   console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}.`);
   console.log(`[Express] Ingresar a http://localhost:${APP_PORT}.\n`);
 });
+
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+
+app.use(
+  session({
+    secret: "elTeamDePepeGrillo",
+    resave: false,
+    saveUninitialized: false,
+  })
+)
+
+app.use(passport.session());
+
+passport.use(new LocalStrategy ({
+  usernameField: 'email',
+  passwordField: 'password'
+}, async function(username, password, done) {
+  try{
+
+  }
+}
+))
